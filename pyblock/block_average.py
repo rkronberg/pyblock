1780a95d85169bfcdce97b63bd6a50c8f64ac04e
Rasmus <rasmus.kronberg@aalto.fi>
2020-08-23 19:57:16 +0300

    Initial commit

---
+++ pyblock/block_average.py
@@ -0,0 +1,66 @@
+"""
+This code computes an error estimate for the average of a 
+correlated dataset using block averaging
+
+See "Computer Simulation of Liquids" by Allen & Tildesley 
+(2nd ed., Oxford University Press, 2017, p. 282-283)
+
+author: Rasmus Kronberg
+email: rasmus.kronberg@aalto.fi
+
+"""
+
+import numpy as np
+from scipy.optimize import curve_fit
+import warnings
+
+class Blocked:
+	def __init__(self, data, bmin):
+
+		self.df = data
+
+		# Mean and variance of full timeseries
+		self.run_ave = np.mean(data)
+		self.run_var = np.var(data, ddof=1)
+	
+		# Length of timeseries and a list of all block sizes
+		self.ntot = len(data)
+		self.sizes = range(1, int(self.ntot/bmin))
+	
+		# Initialize blocked variances
+		self.vars = np.zeros(len(self.sizes))
+
+	# Block average
+	def run(self):
+
+		for i, size in enumerate(self.sizes):
+			print('Processing blocks %.1f %%' % (100*i/len(self.sizes)), end='\r')
+
+			# Truncate data appropriately from the beginning
+			mod = self.ntot%size
+			tmp = self.df[mod:]
+
+			# Reshape so that each row corresponds a block
+			nblocks = int(self.ntot/size)
+			tmp = tmp.reshape((nblocks, size))
+
+			# Average blocks
+			block_aves = np.mean(tmp, axis=1)
+
+			# Compute variance of block averages
+			self.vars[i] = np.var(block_aves, ddof=1)
+
+	# Statistical inefficiency
+	def error(self):
+
+		self.s = self.sizes*self.vars/self.run_var
+		self.fit = lambda x, a, b: a*(1-np.exp(-b*x))
+
+		# Fit asymptotic function to s
+		with warnings.catch_warnings():
+			warnings.simplefilter('ignore')
+			self.popt, self.pcov = curve_fit(self.fit, self.sizes, self.s)
+		
+		# Statistical inefficiency given by the limit as x -> inf (popt[0])
+		self.err = np.sqrt(self.run_var*self.popt[0]/self.ntot)
+		
---
+++ pyblock/main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+from block_average import Blocked
+
+def parse():
+
+	# Parse command line arguments
+
+	p = ArgumentParser(description='Error estimation by block averages')
+	p.add_argument('-i', '--input', required=True, help='Input file')
+	p.add_argument('-n', '--bmin', default=10, type=int, help='Smallest number of blocks')
+	p.add_argument('-p', '--plot', action='store_true', help='Plot statistical inefficiency fit')
+
+	return vars(p.parse_args())
+
+def main():
+
+	args = parse()
+
+	print('Computing average with error estimate for correlated timeseries %s' % args['input'])
+	
+	# Load data
+	data = np.loadtxt(args['input'])
+
+	# Proceed with the block averaging
+	blk = Blocked(data, args['bmin'])
+	blk.run()
+
+	# Compute an error estimate for the average
+	blk.error()
+
+	print('\n<x> = %.4f +/- %.4f' % (blk.run_ave, blk.err))
+
+	# Visual evaluation
+	if(args['plot']):
+		plt.plot(blk.sizes, blk.s)
+		plt.plot(blk.sizes, blk.fit(blk.sizes, *blk.popt))
+		plt.axhline(blk.popt[0])
+		plt.show()
+
+if __name__ == '__main__':
+	main()
+	
